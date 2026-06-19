fcd0a2cadcc018faf6cb3ac13ddd62621ea00486
DanniHel <132689177+DanniHel@users.noreply.github.com>
2026-06-19 08:54:46 -0500

    Update router history base URL to VITE_API_BASE_URL

--- src/router/index.js
+++ src/router/index.js
@@ -2,7 +2,7 @@ import { createRouter, createWebHistory } from 'vue-router'
 import ConstanciaView from '../views/ConstanciaView.vue'
 
 const router = createRouter({
-  history: createWebHistory(import.meta.env.BASE_URL),
+  history: createWebHistory(import.meta.env.VITE_API_BASE_URL),
   routes: [
     {
       path: '/constancia/:cui',
